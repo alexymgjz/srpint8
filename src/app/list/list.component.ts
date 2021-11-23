d0d3a50e84e9ee0044295b1d48e267ed4f4bdcc6
alexys <alexysmgjz@gmail.com>
2021-11-23 19:45:03 +0100

    20%

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,17 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 import {WarService} from '../war.service';
-
+export interface datosParaVistaI{
+  "name": string,
+  "model": string
+}
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
   styleUrls: ['./list.component.scss']
 })
 export class ListComponent implements OnInit {
+  datos:any;
+  constructor(public war:WarService) {
 
-  constructor(public war:WarService) { }
+  }
 
   ngOnInit() {
-
+    this.war.getData(1).subscribe(data => {
+     this.datos = data
+     console.log(this.datos.result);
+    })
   }
 
 }
--- src/app/war.service.ts
+++ src/app/war.service.ts
@@ -1,9 +1,10 @@
 import { Injectable } from '@angular/core';
 import {HttpClient } from "@angular/common/http";
-import {Observable, Subject} from "rxjs";
+import {Observable} from "rxjs";
+
 
 //1 definimos el tipo
- export interface Nave{
+ export interface DataTotalI{
    "count":number,
    "next": string,
    "previous": string,
@@ -26,7 +27,10 @@ import {Observable, Subject} from "rxjs";
        "created": string,
        "edited": string,
        "url": string
-    } ]
+    } ],
+   "created": string,
+   "edited": string,
+   "url": string
  }
 
 
@@ -35,48 +39,15 @@ import {Observable, Subject} from "rxjs";
 })
 export class WarService {
  url:string='https://swapi.dev/api/starships/?page=';
- pagination:number=1;
-  frases: {};
 
-   //2 creamos el array del tipo creado en la interfase
-    private naves: Nave[];
 
-   //4 creamos el subject usamos el mismo nombre de la variable pero con signo ($) como estandar ;
-    // el tipo de subject a emitir en este caso es el array de naves (:Subject<Nave[]>)
-    private nave$ :Subject<Nave[]>;
 
-  constructor(private http:HttpClient) {
-    //3 inicializamos el array vacio
-    this.naves =[];
-     this.frases= {};
-    //inicializamos o creamos eel subject
-    this.nave$ = new Subject();
+  constructor(private http:HttpClient) { }
 
+
+  getData(page:number):Observable<DataTotalI[]>{
+    return this.http.get<DataTotalI[]>(`${this.url} + ${page}`)
   }
-  getData(){
-    let result:any;
-    this.http.get(`${this.url} + ${this.pagination}`).subscribe(result => {
-          console.log('aaaaaa', result);
-          return result;
-        },
-        error => {
-         return [];
-        }
-      );
-  }
-
-  //creamos los servicios
-  addNave(pnave:Nave){
-  this.naves.push(pnave);
-
-  //notificamos atrevez del subject que ha sucedido un cambio y con el next mandamos de nuevo el array
-  this.nave$.next(this.naves);
-}
-
-//ahora creamos un observasable para recivirlo en los otros modulos
-   addNaves$():Observable<Nave[]>{
-     return this.nave$.asObservable();
-}
 
 
 }
